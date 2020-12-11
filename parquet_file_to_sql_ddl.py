56568ac6028ad3969861c9b375df5a93cbaaa0d3
Blake Enyart <blake.enyart@nutrien.com>
2020-12-10 17:06:16 -0700

    Add updated files and new features for CDK Pipelines

--- parquet_file_to_sql_ddl.py
+++ parquet_file_to_sql_ddl.py
@@ -6,9 +6,12 @@ import csv
 file_path = "/Users/bl9190033/Downloads/data-pipeline-practice-MegaDataPipelineKinesisFireh-ODItgGklWG78-8-2020-12-06-07-28-16-bef43b04-bcfb-4f26-92d2-804f97381591.parquet"
 table_name = "test"
 sql_dialect = "postgresql"
+file_output = "test.sql"
 
 
-def parquet_to_sql_ddl(file_path: str, table_name: str, sql_dialect: str):
+def parquet_to_sql_ddl(
+    file_path: str, table_name: str, sql_dialect: str, file_output: str
+):
     f = io.StringIO()
 
     df = pd.read_parquet(file_path)
@@ -17,9 +20,9 @@ def parquet_to_sql_ddl(file_path: str, table_name: str, sql_dialect: str):
     table = Table(data=data, table_name=table_name, uniques=True)
     sql = table.sql(sql_dialect, drops=False, metadata_source=True)
 
-    with open(file="test.sql", mode="w") as output:
+    with open(file=file_output, mode="w") as output:
         output.write(sql)
 
 
 if __name__ == "__main__":
-    parquet_to_sql_ddl(file_path, table_name, sql_dialect)
+    parquet_to_sql_ddl(file_path, table_name, sql_dialect, file_output)
